9ca909868545b38bfa27291ca7148b2f46e3f34a
Chance <chanceyoungwebdev@gmail.com>
2022-01-22 16:15:11 -0700

    reformatted date string for postgres

--- web/src/models/wgpeer.js
+++ web/src/models/wgpeer.js
@@ -1,7 +1,7 @@
 export default function wgpeer(name, ip, allowed_range, pubkey, privkey, date) {
     this.client_name = name
     this.ip_address = ip
-    this.date_added = date || Date.now()
+    this.date_added = date || new Date().toISOString().slice(0, 10)
     this.allowed_ip_range = allowed_range
     this.client_public_key = pubkey
     this.client_private_key = privkey
