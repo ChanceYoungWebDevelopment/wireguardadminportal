6c9b42323179a4ab117c4967697c88d8e73ccca7
Chance <chanceyoungwebdev@gmail.com>
2022-01-26 10:51:12 -0700

    fixed silly sql mess

--- api/index.js
+++ api/index.js
@@ -27,38 +27,21 @@ app.post('/addpeer', (req, res) => {
         privkey: req.body.client_private_key,
     }
 
-    const querystring =
-        'insert into wireguard_info(client_name,ip_address,date_added,allowed_ip_range,client_pubkey,client_privkey) values(' +
-        "'" +
-        peer.name +
-        "'" +
-        ',' +
-        "'" +
-        peer.ip +
-        "'" +
-        ',' +
-        "'" +
-        peer.date +
-        "'" +
-        ',' +
-        "'" +
-        peer.allowed +
-        "'" +
-        ',' +
-        "'" +
-        peer.pubkey +
-        "'" +
-        ',' +
-        "'" +
-        peer.privkey +
-        "'" +
-        ')'
-
-    pool.query(querystring, (err, resp) => {
+    const query = `insert into wireguard_info(client_name,ip_address,date_added,allowed_ip_range,client_pubkey,client_privkey) 
+                    values($1, $2, $3,$4,$5,$6)`
+    const values = [
+        peer.name,
+        peer.ip,
+        peer.allowed,
+        peer.date,
+        peer.pubkey,
+        peer.privkey,
+    ]
+    pool.query(query, values, (err, resp) => {
         if (err) {
             console.log(err)
-        }
-        res.sendStatus(200)
+            res.sendStatus(500)
+        } else res.sendStatus(200)
     })
 })
 
