ff48db2b27dcbaadd5849bfb0a555759ac81ee25
Chance <chanceyoungwebdev@gmail.com>
2022-01-29 19:50:36 -0700

    fixed returning value

--- api/dbservice.js
+++ api/dbservice.js
@@ -7,7 +7,7 @@ const addPeerData = async (passed_peer) => {
     }
 
     const query = `insert into wireguard_info(client_name,ip_address,date_added,allowed_ip_range,client_pubkey,client_privkey) 
-                    values($1, $2, $3,$4,$5,$6) returning ip_adress, client_pubkey`
+                    values($1, $2, $3,$4,$5,$6) returning ip_address, client_pubkey`
     const values = [
         peer.name,
         peer.ip,
