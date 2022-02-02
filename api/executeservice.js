57bc559633bb55582f59f8f0dacb76a34594c628
Chance <chanceyoungwebdev@gmail.com>
2022-02-02 15:22:06 -0700

    forgot to add require for uuid

--- api/executeservice.js
+++ api/executeservice.js
@@ -1,5 +1,4 @@
 const { execSync } = require('child_process')
-const { uuid } = require('uuidv4')
 
 const getWireguardStatus = () => {
     return execSync('systemctl status wg-quick@wg0.service', {
