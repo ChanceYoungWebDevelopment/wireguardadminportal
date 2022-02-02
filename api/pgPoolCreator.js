fb3e081309cb3980c2610ca37bc3dd5bc76e4391
Chance <chanceyoungwebdev@gmail.com>
2022-02-02 16:34:53 -0700

    added debugging for poolcreator

--- api/pgPoolCreator.js
+++ api/pgPoolCreator.js
@@ -1,5 +1,7 @@
 const { Pool } = require('pg')
 const connectionString = process.env.WGA_POSTGRES_CONNECTION
+console.log('prepool')
 const pool = new Pool({ connectionString })
+console.log('postpool')
 
 module.exports = pool
