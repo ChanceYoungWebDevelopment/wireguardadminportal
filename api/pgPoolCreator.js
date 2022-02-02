fd4325c78344168bcadbf346f4a45a71ab9d5ead
Chance <chanceyoungwebdev@gmail.com>
2022-02-02 11:44:13 -0700

    capitals.

--- api/pgPoolCreator.js
+++ api/pgPoolCreator.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg')
-const connectionstring = process.env.WGA_POSTGRES_CONNECTION
-const pool = new Pool({ connectionstring })
+const connectionString = process.env.WGA_POSTGRES_CONNECTION
+const pool = new Pool({ connectionString })
 
 module.exports = pool
