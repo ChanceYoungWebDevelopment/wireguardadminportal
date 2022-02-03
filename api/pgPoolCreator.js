9b24d6973db226d6a8e35577c821d86d7427a4b8
Chance <chanceyoungwebdev@gmail.com>
2022-02-02 22:10:31 -0700

    reverted to connection object and added process.yml

--- api/pgPoolCreator.js
+++ api/pgPoolCreator.js
@@ -1,8 +1,11 @@
-const { Pool } = require('pg')
-const connectionString = process.env.WGA_POSTGRES_CONNECTION
-console.log('prepool')
-const pool = new Pool({ connectionString })
-console.log('postpool')
-pool.connect().then(console.log('postconnection'))
+const Pool = require('pg').Pool
+
+const pool = new Pool({
+    user: process.env.WGA_POSTGRES_USER,
+    host: process.env.WGA_POSTGRES_HOST,
+    database: process.env.WGA_POSTGRES_DB,
+    password: process.env.WGA_POSTGRES_PASSWORD,
+    port: 5432,
+})
 
 module.exports = pool
