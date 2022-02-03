4ebda2ad986d3b1e8066695b62fecbe66db0a4e5
Chance <chanceyoungwebdev@gmail.com>
2022-02-02 23:28:35 -0700

    added seventh paramter for query

--- api/dbservice.js
+++ api/dbservice.js
@@ -7,7 +7,7 @@ const addPeerData = async (passed_peer) => {
     }
 
     const query = `insert into wireguard_info(client_name,ip_address,date_added,allowed_ip_range,client_pubkey,client_privkey,client_uuid) 
-                    values($1, $2, $3,$4,$5,$6) returning *`
+                    values($1, $2, $3,$4,$5,$6,$7) returning *`
     const values = [
         peer.name,
         peer.ip,
